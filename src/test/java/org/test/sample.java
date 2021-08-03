0f784868859a5c86f22fd5b7399b021819e5306a
swarnasaravanan <slathasaravanan11@gmail.com>
2021-08-03 10:47:03 +0530

    hari code

--- src/test/java/org/test/sample.java
+++ src/test/java/org/test/sample.java
@@ -1,5 +1,13 @@
 package org.test;
 
-public class sample {
+import org.base.BaseClass;
+
+public class sample extends BaseClass {
+	public static void main(String[] args) {
+		System.out.println("Test1");
+		System.out.println("Test2");
+		
+		
+	}
 
 }
